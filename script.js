0108b9f4560fb860a9a1381caba426edabd92b81
nouman ahmad <88377941+nomi22138@users.noreply.github.com>
2024-06-30 02:53:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+document.getElementById('decreaseBtn').onclick = function(){
+    count -=1;
+    document.getElementById("countlabel").innerHTML = count;
+
+}
+
+document.getElementById('resetBtn').onclick = function(){
+    count =0;
+    document.getElementById("countlabel").innerHTML = count;
+}
+
+document.getElementById('increaseBtn').onclick = function(){
+    count +=1;
+    document.getElementById("countlabel").innerHTML = count;
+}
